e8e50ba251a54e7ba460d9e5db5831269b2fd31b
JoDagnault <jdagnault@gmail.com>
2026-06-24 01:29:12 -0400

    Add mdx dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import { defaultLocale, locales } from "./src/i18n/locales.ts";
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
   site: "https://jodagnault.com/",
+
   i18n: {
     locales,
     defaultLocale,
@@ -11,4 +14,6 @@ export default defineConfig({
       redirectToDefaultLocale: false,
     },
   },
+
+  integrations: [mdx()],
 });
